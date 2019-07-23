30bffef213a440f5a0a836e5159362ee2370e24f
kika2001 <kika.teixeira2001@gmail.com>
2019-07-23 11:17:00 +0100

    d

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,7 +1,14 @@
-        <header class="main_menu_area">
-            <nav class="navbar navbar-expand-lg -light">
+<header class="main_menu_area">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <a class="navbar-brand" href="#"><img src="img/logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
 
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
             <?php
             
             
@@ -13,18 +20,14 @@
             ?>
 
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav">
                         <li class="nav-item"><a class="nav-link" href="<?php echo $value['link'] ?>"><?php echo $value['nome'] ?></a></li>
-                    </ul>
-                </div>
+                 
             <?php } ?>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="admin/login.php">login</a></li>
-            </ul>
-            </div>
-
+            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
             </nav>
         </header>
+
+       
